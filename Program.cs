98726c6e9186d5f4b36c831533cb079f0540e8ce
syedfaizz <syedfaizz45@gmail.com>
2021-08-03 18:58:36 +0530

    [faisal] Add: uc1-maxinteger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Findmaxgenerics
+namespace FindMaximumGenerics
 {
     class Program
     {
@@ -10,12 +10,7 @@ namespace Findmaxgenerics
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Max value among these three integer");
-            Maximum<int>.GetMax(10, 20, 30);
-            Console.WriteLine("Max value among these three floats");
-            Maximum<double>.GetMax(10.2f, 35.1f, 30.5f);
-            Console.WriteLine("Max value among these three string");
-            Maximum<string>.GetMax("Apple", "Peach", "Banana");
+            Maximum.GetMax(10, 20, 15);
         }
     }
 }
---
+++ maximum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Findmaxgenerics
+{
+    class Maximum
+    {
+        public static void GetMax(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            //// used compareto method
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+                Console.WriteLine(firstNumber + ", Is the max value.");
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+                Console.WriteLine(secondNumber + ", Is the max value.");
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+                Console.WriteLine(thirdNumber + ", Is the max value.");
+        }
+    }
+}
