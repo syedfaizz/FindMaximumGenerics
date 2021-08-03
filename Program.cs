f30fac83ef40369df3ec81669bab695ad0e5a41a
syedfaizz <syedfaizz45@gmail.com>
2021-08-03 18:53:19 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Findmaxgenerics
+{
+    class Program
+    {
+        /// <summary>
+        /// execution start from here!
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max value among these three integer");
+            Maximum<int>.GetMax(10, 20, 30);
+            Console.WriteLine("Max value among these three floats");
+            Maximum<double>.GetMax(10.2f, 35.1f, 30.5f);
+            Console.WriteLine("Max value among these three string");
+            Maximum<string>.GetMax("Apple", "Peach", "Banana");
+        }
+    }
+}
